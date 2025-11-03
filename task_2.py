ecefb0df53428bef0d609bc54f1876a484ab0941
RomanBirov <roman0630565956@gmail.com>
2025-11-03 15:18:01 +0200

    change reg in pattern change logic \b to _

--- task_2.py
+++ task_2.py
@@ -2,12 +2,9 @@ import re
 from typing import Callable
 
 def generator_numbers(text: str):
-    pattern = r"\b\d+\.\d+\b"
+    pattern = r'(?<= )[+-]?\d+(?:\.\d+)?(?= )'
     for match in re.finditer(pattern, text):
-        yield float(match.group())  
+        yield float(match.group())
 
 def sum_profit(text: str, func: Callable):
-    total = sum(
-        func(text)
-    )
-    return total
+    return round(sum(func(text)), 2)
